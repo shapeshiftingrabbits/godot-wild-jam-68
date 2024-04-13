5055d4d374d7c3d6c3dcc21994324c0ef55ea0b3
Laurie Trichet <laurie.trichet@protonmail.com>
2024-04-13 17:42:17 +1000

    add a done signal with a done state label

--- scripts/tool.gd
+++ scripts/tool.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-@onready var sprites = [$Sprite2D, $Sprite2D2,$Sprite2D3]
+signal done
+
 var index = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for sprite in sprites:
-		sprite.hide()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,16 +15,11 @@ func _process(delta):
 func update_visible_sprite():
 	iterate_over_animated_sprite()
 
-func iterate_over_sprite_array():
-	for sprite in sprites:
-		sprite.hide()
-	sprites[index].show()
-	var nbOfSprites = sprites.size()
-	index = (index + 1)% nbOfSprites
-	
 func iterate_over_animated_sprite():
 	var animation =  $AnimatedSprite2D.animation
 	var nbOfSprites = $AnimatedSprite2D.sprite_frames.get_frame_count(animation)
-	index = (index + 1)% nbOfSprites
+	index = clamp( index + 1, 0, nbOfSprites - 1)
 	$AnimatedSprite2D.frame = index
-	
+	if (index == nbOfSprites - 1 ):
+		done.emit()
+
--- scripts/udpate_tool_state.gd
+++ scripts/udpate_tool_state.gd
@@ -3,13 +3,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$DoneLabel.hide()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_tool_used():
-	$TargetObjectMaking.update_visible_sprite()
+	$ForgeableItem.update_visible_sprite()
 
+func _on_forgeable_item_done():
+	$DoneLabel.show()
