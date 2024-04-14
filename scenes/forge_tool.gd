5ef265fe259d326a7fca15eaa976eb183416c254
jesterfraud <107968326+jesterfraud@users.noreply.github.com>
2024-04-14 20:59:30 +1000

    Prevent double-hits

--- scenes/forge_tool.gd
+++ scenes/forge_tool.gd
@@ -6,17 +6,26 @@ signal hit
 
 var time: float = 0
 var interactible = true
+var hit_this_loop = false
 
 func _process(delta):
+	var mod_before = fmod(time, loop_time_seconds)
+
 	time += delta
 
 	%Current.rotation = fmod(time / loop_time_seconds, 1.0) * TAU
+	# TODO: probably should be elsewhere
 	%Target.difficulty = difficulty
+
+	if fmod(time, loop_time_seconds) < mod_before:
+		hit_this_loop = false
 
 func _input(event):
 	if interactible && event is InputEvent:
 		if event.is_action_pressed("minigame_interact") && in_target_time():
-			hit.emit()
+			if not hit_this_loop:
+				hit_this_loop = true
+				hit.emit()
 
 func in_target_time():
 	var target_time_duration = loop_time_seconds / difficulty
