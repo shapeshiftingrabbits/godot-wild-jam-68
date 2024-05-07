3732ba8420f7bbf51ba9e251318a8d7da5f2e89e
Laurie Trichet <laurie.trichet@protonmail.com>
2024-05-08 08:07:35 +1000

    attach big project completion tracker script

--- scripts/commands/track_big_project_completion.gd
+++ scripts/commands/track_big_project_completion.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal big_project_updated
+
 var ledger: Array[LedgerItem]
 
 @export var big_projects_dialogue:DialogueData
@@ -13,4 +15,7 @@ func extract_ledger_from_dialogue():
 	
 	var big_project: BigProject = load("res://data/big_projects/%s" % big_project_id)
 	ledger = big_project.loots.map(func (loot: Loot): LedgerItem.new(loot.loot_item, loot.amount))
-	
+
+
+func _on_run_dialogue_big_project_presented(big_project_dialogue):
+	extract_ledger_from_dialogue()
