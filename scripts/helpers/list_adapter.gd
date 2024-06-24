34a7e7b350f7973e453d6d5fae8f5efe10176f51
Laurie Trichet <laurie.trichet@protonmail.com>
2024-06-24 14:30:29 +1000

    refactor code for the creation of list views
    - add documentation for the list_adapter class.
    - replace the cell creation code with the list adapter.

--- scripts/helpers/list_adapter.gd
+++ scripts/helpers/list_adapter.gd
@@ -2,36 +2,58 @@ extends RefCounted
 
 class_name ListAdapter
 
+## Gives a set of behaviors to create cells for a list of models.
+## 
+## Handles adding and updating cells with models.
+## Initialise giving:
+## - [param in_models]: [Dictionary] the models dictionary
+## - [param in_list_view]: [Container] to add the cells to
+## - [param cell_packed_scene]: [PackedScene], a [Container] to be instanciated to create the cell.
+## - [param in_bind_model] has 3 parameters
+##		- [param cell]: [Container], 
+##		- [param model_key]: [any] the key for the item in the dictionary, 
+##		- [param model]: [any] the model itself that will be attached to the cell as a meta
+##		[codeblock]
+##			func bind_model(cell: Container, model_key: any , model: any):
+##				pass
+##		[/codeblock]
+
+
 var list_view : Container
 var models : Dictionary
 var bind_model: Callable
-var add_new_cell: Callable
+var cell_packed_scene: PackedScene
 
 const MODEL_KEY = "model_key"
 
-func _init(in_models: Dictionary, in_list_view: Container, in_add_new_cell: Callable, in_bind_model: Callable):
+
+func _init( in_models: Dictionary, 
+			in_list_view: Container, 
+			in_cell_packed_scene: PackedScene, 
+			in_bind_model: Callable):
 	models = in_models 
 	list_view = in_list_view
-	add_new_cell = in_add_new_cell
+	cell_packed_scene = in_cell_packed_scene
 	bind_model = in_bind_model
+	create_cells()
 
 
-## @param add_new_cell must return a [Container] to be added to the list.
-## @param bind_model takes 3 args, 
-## 	the cell, 
-## 	the key for the item, 
-## 	and the item itself
-func create_cells( add_new_cell: Callable):
+func create_cells():
 	for model_key in models:
-		var entry = models.get(model_key)
-		var cell: Container = add_new_cell.call()
-		cell.set_meta(MODEL_KEY, entry)
-		bind_model.call(cell, model_key, entry)
+		var model = models.get(model_key)
+		var cell = add_cell_to_list(model_key, model)
 		list_view.add_child(cell)
 		list_view.set_meta(model_key, cell)
 
 
-func update_grid():
+func add_cell_to_list(model_key, model):
+	var cell: Container = cell_packed_scene.instantiate()
+	cell.set_meta(MODEL_KEY, model)
+	bind_model.call(cell, model_key, model)
+	return cell
+	
+
+func update_existing_cells():
 	var cells = list_view.get_children()
 	for cell in cells:
 		var cell_model_key = cell.get_meta(MODEL_KEY)
@@ -40,12 +62,9 @@ func update_grid():
 			bind_model.call(cell, cell_model_key, entry)
 
 
-func _update_item(in_name: String, in_amount: int):
-	var cell = grid_container.get_meta(in_name)
+func update_item(model_key: String, model: int):
+	var cell = list_view.get_meta(model_key)
 	if (cell == null):
-		_add_new_cell(in_name, in_amount)
+		add_cell_to_list(model_key, model)
 	else:
-		cell.update_amount(in_amount)
-
-
-
+		cell.update_amount(model)
--- scripts/inventory/inventory_view.gd
+++ scripts/inventory/inventory_view.gd
@@ -5,42 +5,17 @@ class_name InventoryView
 @export var inventory: Inventory
 @export var cell_template: PackedScene
 @onready var grid_container = %GridContainer
-
-const LOOT_KEY = "loot"
+var list_adapter: ListAdapter
 
 func _ready():
-	inventory.inventory_updated.connect(_update_item)
 	hide()
-	fill_grid()
-
-func fill_grid():
-	var loots = inventory.loot_dictionary
-	for loot in loots:
-		_add_new_cell(loot, loots[loot])
+	list_adapter = ListAdapter.new(inventory.loot_dictionary, grid_container, cell_template, _bind_model )
+	inventory.inventory_updated.connect(list_adapter.update_item)
 
 
-func _add_new_cell(name: String, amount: int):
-	var cell: InventoryCell = cell_template.instantiate()
-	cell.set_values( name, amount)
-	cell.set_meta(LOOT_KEY, name)
-	grid_container.add_child(cell)
-	grid_container.set_meta(name, cell)
+func _bind_model(cell: Container, model_key: String , model: int):
+	cell.set_values( model_key, model)
 
-func update_grid():
-	var cells = grid_container.get_children()
-	for cell in cells:
-		var cell_loot = cell.get_meta(LOOT_KEY)
-		if (inventory.loot_dictionary.has(cell_loot)):
-			var inventory_entry = inventory.loot_dictionary.get(cell_loot)
-			cell.update_amount(inventory_entry)
-
-
-func _update_item(in_name: String, in_amount: int):
-	var cell = grid_container.get_meta(in_name)
-	if (cell == null):
-		_add_new_cell(in_name, in_amount)
-	else:
-		cell.update_amount(in_amount)
 
 func _on_button_pressed():
 	hide()
